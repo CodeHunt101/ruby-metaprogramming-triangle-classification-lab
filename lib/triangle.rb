613ab026561e68d817607549f1996d68333f9aa9
Harold Torres Marino <haroldtm55@gmail.com>
2021-08-23 17:26:37 +1000

    Lab completed

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
-  # write code here
+  attr_reader :side_1, :side_2, :side_3
+  def initialize(side_1,side_2,side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind
+    if @side_1 == @side_2 && 
+      @side_2 == @side_3 && 
+      (side_1 >0 && @side_2 > 0 && @side_3 > 0) &&
+      (side_1 + side_2 > side_3) && (side_2 + side_3 > side_1) && (side_1 + side_3 > side_2)
+      :equilateral
+    elsif (((@side_1 == @side_2) && (@side_2 != @side_3 || @side_1 != @side_3)) || 
+      ((@side_1 == @side_3) && (@side_3 != @side_2 || @side_1 != @side_2)) ||
+      ((@side_2 == @side_3) && (@side_3 != @side_1 || @side_2 != @side_1))) && 
+      (@side_1 > 0 && @side_2 > 0 && @side_3 > 0) &&
+      (side_1 + side_2 > side_3) && (side_2 + side_3> side_1) && (side_1 + side_3 > side_2)
+      :isosceles
+    elsif @side_1 != @side_2 && @side_2 != @side_3 && 
+      (@side_1 > 0 && @side_2 > 0 && @side_3 > 0) &&
+      (side_1 + side_2 > side_3) && (side_2 + side_3> side_1) && (side_1 + side_3 > side_2)
+      :scalene
+    # elsif @side_1 == 0 && @side_2 == 0 && @side_3 == 0
+    #   raise TriangleError
+    # elsif @side_1 < 0 || @side_2 < 0 || @side_3 < 0
+    #   raise TriangleError
+    else raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
