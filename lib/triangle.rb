748d317c5669e7a278b27d0cc060a6235aef0179
Harold Torres Marino <haroldtm55@gmail.com>
2021-08-23 17:44:00 +1000

    Lab completed and improved

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,9 +12,9 @@ class Triangle
       (side_1 >0 && @side_2 > 0 && @side_3 > 0) &&
       (side_1 + side_2 > side_3) && (side_2 + side_3 > side_1) && (side_1 + side_3 > side_2)
       :equilateral
-    elsif (((@side_1 == @side_2) && (@side_2 != @side_3 || @side_1 != @side_3)) || 
-      ((@side_1 == @side_3) && (@side_3 != @side_2 || @side_1 != @side_2)) ||
-      ((@side_2 == @side_3) && (@side_3 != @side_1 || @side_2 != @side_1))) && 
+    elsif ((@side_1 == @side_2 && @side_2 != @side_3) || 
+      (@side_1 == @side_3 && @side_3 != @side_2) ||
+      (@side_2 == @side_3 && @side_3 != @side_1)) && 
       (@side_1 > 0 && @side_2 > 0 && @side_3 > 0) &&
       (side_1 + side_2 > side_3) && (side_2 + side_3> side_1) && (side_1 + side_3 > side_2)
       :isosceles
@@ -22,10 +22,6 @@ class Triangle
       (@side_1 > 0 && @side_2 > 0 && @side_3 > 0) &&
       (side_1 + side_2 > side_3) && (side_2 + side_3> side_1) && (side_1 + side_3 > side_2)
       :scalene
-    # elsif @side_1 == 0 && @side_2 == 0 && @side_3 == 0
-    #   raise TriangleError
-    # elsif @side_1 < 0 || @side_2 < 0 || @side_3 < 0
-    #   raise TriangleError
     else raise TriangleError
     end
   end
